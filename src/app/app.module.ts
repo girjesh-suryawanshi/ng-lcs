910c6be74645d1997386e259c859dc8c9e78b1c3
Girjesh <girjeshlt@gmail.com>
2019-08-25 22:43:37 +0530

    login service created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { LcsModule } from './lcs/lcs.module';
+import { FormsModule }   from '@angular/forms';
+import { LoginServiceService } from './login-service.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,9 +18,12 @@ import { RegistrationComponent } from './registration/registration.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LcsModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [LoginServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginServiceService } from '../login-service.service';
 
 @Component({
   selector: 'login',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+ message:string ;
+
+ number:number=4;
+
+ userName:string;
+  
+
+ constructor(private loginservice:LoginServiceService){
+
+  setInterval(()=>{
+    let date = new Date();
+
+    this.message = date.toLocaleDateString() +" " + date.toLocaleTimeString()  },1000)
+  
+ }
 
   ngOnInit() {
+  
   }
 
+  binding(){
+    console.log("binding click");
+   this.loginservice.getAllUser().subscribe(success=>{
+     console.log("inside success");
+     console.log(success);
+   },error=>{})
+  }
+
+  
 }
